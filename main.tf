de48d1675bea58c1d56a91a3dc8c1e0ef4a8e78e
kundan singh <kundanec01@gmail.com>
2025-08-04 08:37:05 +0530

    main branch of todoinfra

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+provider "azurerm" {
+ features {}
+ subscription_id = "4874f019-c697-4254-8925-749b8f7c89e5"
+}
